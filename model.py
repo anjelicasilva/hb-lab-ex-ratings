3627dc34eee8b3210d992c8be9e3ec6f42c0af3d
Anjelica <anjelicacsilva@gmail.com>
2019-12-19 20:59:41 -0800

    Load movies from u.item into ratings database

--- model.py
+++ model.py
@@ -18,8 +18,8 @@ class User(db.Model):
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    email = db.Column(db.String(64), nullable=True)
-    password = db.Column(db.String(64), nullable=True)
+    email = db.Column(db.String(100), nullable=True)
+    password = db.Column(db.String(100), nullable=True)
     age = db.Column(db.Integer, nullable=True)
     zipcode = db.Column(db.String(15), nullable=True)
 
@@ -33,9 +33,9 @@ class Movie(db.Model):
     __tablename__ = "movies"
 
     movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String(64), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     released_at = db.Column(db.DateTime, nullable=False)
-    imdb_url = db.Column(db.String(64), nullable=False)
+    imdb_url = db.Column(db.String(1000), nullable=False)
 
 class Rating(db.Model):
     """Ratings of ratings website."""
